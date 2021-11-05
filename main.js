d924f46e910c83ea86de480668cfdfdf2d30a299
F1xa <dubineckiy@bk.ru>
2021-11-05 05:35:18 +0300

    changet: input of the arguments

--- main.js
+++ main.js
@@ -1,39 +1,71 @@
-const num1 = +prompt('Введите первое чило', '');
-const num2 = +prompt('Введите второе чило', '');
-const operator = prompt('Введите оператор', '');
+// const num1 = +prompt('Введите первое чило', '');
+// const num2 = +prompt('Введите второе чило', '');
+// const operator = prompt('Введите оператор', '');
+// console.log(num1)
 
+// function calc() {
 
-function calc() {
-  if (num1 != null && num2 != null && num1 != '' && num2 != '' && !isNaN(num1) && !isNaN(num2)) {
+//   const isValid = (num1 !== null && num2 !== null && !isNaN(num1) && !isNaN(num2));
+//   if (isValid) {
+   
+//     switch (operator) {
+//         case '+':
+//           alert(num1 + num2);
+//           break
+//         case '-':
+//           alert(num1 - num2);
+//           break
+//         case '*':
+//           alert(num1 * num2);
+//           break
+//         case '/':
+//           alert(num1 / num2);
+//           break;
+//         case '%':
+//           alert(num1 % num2);
+//           break;
+//         case '**':
+//           alert(num1 ** num2);
+//         default:
+//           alert('unknown operation');
+//       }
 
-    if (operator === '+' || operator === '-' || operator === '*' || operator === '/' || operator === '%' || operator === '**') {
-      switch (operator) {
-        case '+':
-          alert(num1 + num2);
-          break
-        case '-':
-          alert(num1 - num2);
-          break
-        case '*':
-          alert(num1 * num2);
-          break
-        case '/':
-          alert(num1 / num2);
-          break;
-        case '%':
-          alert(num1 % num2);
-          break;
-        case '**':
-          alert(num1 ** num2);
-      }
-    } else {
-      alert('unknown operation');
-    }
-
-  } else {
-    alert('Error');
-  }
+//   } else {
+//       alert('Error');
+//     }
+// }
   
-}
+// calc();
 
-calc();
+
+
+function calc(a, b, operator) {
+
+  const isValid = (typeof a === 'number' && typeof b === 'number' && a !== null && b !== null && !isNaN(a) && !isNaN(b))
+
+
+  if (isValid) {
+    
+    switch (operator) {
+      case 'sum':
+        return a + b
+        break
+      case 'sub':
+        return a - b
+        break
+      case 'mult':
+          return a * b
+          break
+      case 'div':
+          return a / b
+          break
+      default:
+        return 'unknown operation'
+    }
+  } else {
+    return 'Error'
+  }
+};
+
+console.log(calc(3, 5, 'sub'));
+

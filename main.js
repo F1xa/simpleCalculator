bb5390a00232d78f684fc9ecc7b97419d61d4f19
F1xa <dubineckiy@bk.ru>
2021-11-06 04:26:44 +0300

    feat: added an object to the functionality

--- main.js
+++ main.js
@@ -1,71 +1,38 @@
-// const num1 = +prompt('Введите первое чило', '');
-// const num2 = +prompt('Введите второе чило', '');
-// const operator = prompt('Введите оператор', '');
-// console.log(num1)
-
-// function calc() {
-
-//   const isValid = (num1 !== null && num2 !== null && !isNaN(num1) && !isNaN(num2));
-//   if (isValid) {
-   
-//     switch (operator) {
-//         case '+':
-//           alert(num1 + num2);
-//           break
-//         case '-':
-//           alert(num1 - num2);
-//           break
-//         case '*':
-//           alert(num1 * num2);
-//           break
-//         case '/':
-//           alert(num1 / num2);
-//           break;
-//         case '%':
-//           alert(num1 % num2);
-//           break;
-//         case '**':
-//           alert(num1 ** num2);
-//         default:
-//           alert('unknown operation');
-//       }
-
-//   } else {
-//       alert('Error');
-//     }
-// }
-  
-// calc();
-
-
 
 function calc(a, b, operator) {
 
-  const isValid = (typeof a === 'number' && typeof b === 'number' && a !== null && b !== null && !isNaN(a) && !isNaN(b))
+  const isValidOperand = (typeof a === 'number' && typeof b === 'number' && a !== null && b !== null && !isNaN(a) && !isNaN(b))
+  const operations = {
+    sum: a + b,
+    sub: a - b,
+    mult: a * b,
+    div: a / b,
+  }
+  const isValidOperator = operator in operations;
 
+  if (isValidOperand && isValidOperator) {
+    return operations[operator]
 
-  if (isValid) {
-    
-    switch (operator) {
-      case 'sum':
-        return a + b
-        break
-      case 'sub':
-        return a - b
-        break
-      case 'mult':
-          return a * b
-          break
-      case 'div':
-          return a / b
-          break
-      default:
-        return 'unknown operation'
-    }
+  } else if (!isValidOperator) {
+    return 'unknown operation'
+
   } else {
     return 'Error'
   }
+  
 };
 
-console.log(calc(3, 5, 'sub'));
+console.log(calc(3, 6, 'sum'));
+
+
+function makeUser(name, age) {
+  return {
+    name,
+    age,
+    height: 30,
+    
+  };
+}
+
+
 
